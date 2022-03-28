1ec8a050cc54d997c1de7e2796607b09d76211b4
Vybhavashri <vybhavashri.ks@gmail.com>
2022-03-28 19:45:43 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,9 +2,7 @@ import { StyleSheet } from "react-native";
 // importing screens we want to navigate between
 import Start from "./components/Start";
 import Chat from "./components/Chat";
-
 import "react-native-gesture-handler";
-
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
